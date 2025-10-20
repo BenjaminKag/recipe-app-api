b17f959748163de83045a47edb5be37227c42509
Benjamin <benjamink0612@gmail.com>
2025-10-20 16:15:30 +0300

    Added PosgreSQL database adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calaculator functions
+"""
+
+
+def add(x, y):
+    """Add x and u and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
